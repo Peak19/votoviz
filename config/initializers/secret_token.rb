7bebabc840c9ad34f494533a89aa72a3e64615cb
Peak19 <amiloff@gmail.com>
2013-08-24 19:25:14 +0000

    initial project version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Votoviz::Application.config.secret_key_base = 'dad50b4113dcbab0e7e9ba2b94bbee76f152a904f10cca0c29a5869028d66d73030d0f3ddc3a6b18916aba858aa391ce2ac9d2634fd66f4ca6e5f8dd50e36d24'
