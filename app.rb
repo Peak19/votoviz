66eee952d70476589f80970fb8daa18a5b38a27a
Alan Gardner <alanctgardner@gmail.com>
2013-08-24 22:10:07 -0700

    Added response text

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ end
 
 get '/survey/:id' do |id|
     survey={:questions => []}
-    pg.exec_params("SELECT surveys.title AS survey_title, organisations.org_name, organisations.location as org_loc, questions.id as question_id, questions.title AS question_text, questions.question_num, responses.choice_id, COUNT(*) as response_count FROM surveys JOIN organisations ON (organisations.id = surveys.org_id) JOIN questions ON (questions.survey_id = surveys.id) JOIN responses ON (responses.question_id = questions.id) WHERE surveys.id=$1 GROUP BY surveys.title, organisations.org_name, organisations.location, questions.id, questions.title, questions.question_num, responses.choice_id", [id]) do |result|
+    pg.exec_params("SELECT surveys.title AS survey_title, organisations.org_name, organisations.location as org_loc, questions.id as question_id, questions.title AS question_text, questions.question_num, responses.choice_id, choices.text AS choice_name, COUNT(*) as response_count FROM surveys JOIN organisations ON (organisations.id = surveys.org_id) JOIN questions ON (questions.survey_id = surveys.id) JOIN responses ON (responses.question_id = questions.id) JOIN choices ON (choices.question_id = questions.id AND choices.id = responses.choice_id) WHERE surveys.id=$1 GROUP BY surveys.title, organisations.org_name, organisations.location, questions.id, questions.title, questions.question_num, responses.choice_id, choices.text", [id]) do |result|
       result.each do |row|
         survey[:title] ||= row['survey_title']
         survey[:org] ||= row['org_name']
@@ -32,7 +32,7 @@ get '/survey/:id' do |id|
         /^(Yes|No)?Q([0-9]+)/.match(row['question_text']) do |match|
             survey[:questions][Integer(row['question_num'])]['question_depends'] = { :question_id => match[2], :question_response => (match[1] == 'Yes' ? 1:2) } 
         end
-        survey[:questions][Integer(row['question_num'])][:responses] << {:id => row['choice_id'], :count => row['response_count']}
+        survey[:questions][Integer(row['question_num'])][:responses] << {:id => row['choice_id'], :text => row['choice_name'], :count => row['response_count']}
       end
     end
     return survey.to_json
